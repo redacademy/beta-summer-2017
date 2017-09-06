2bb3c6794d55960bbf98521bc7cdad39ce0c76fc
tmclean85 <trevor.a.mclean@gmail.com>
2017-09-06 14:19:20 -0700

    Correct spelling and casing in filenames, implement basic landing page

---
+++ js/scenes/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const LandingPage = () => {
+  <View>
+    <Text>This is the landing page. </Text>
+  </View>  
+}
+
+export default LandingPage;
---
+++ js/scenes/LandingPage/LandingpageContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import LandingPage from './LandingPage';
+
+class LandingPageContainer extends Component {
+
+
+  render() {
+    return <LandingPage />;
+  }
+}
+
+export default LandingPageContainer;
