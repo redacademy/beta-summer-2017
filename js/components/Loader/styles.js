eeb45a13914fdb168d425df8bbcb33afd394dd4f
Sonia <soniasophiaw@gmail.com>
2017-09-06 16:31:55 -0700

    Create Loader component

---
+++ js/components/Loader/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    activityIndicator: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 80
+    }
+});
