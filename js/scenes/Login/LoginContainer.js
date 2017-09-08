5324e890a62b519bf3ce13368da281433a301f4e
heyliddle <eric.liddle@gmail.com>
2017-09-07 18:09:49 -0700

    Attempting to resolve issues with push login scene again

--- js/scenes/Login/LoginContainer.js
+++ js/scenes/Login/LoginContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Login from './Login';
 
 class LoginContainer extends Component {
@@ -12,16 +11,3 @@ class LoginContainer extends Component {
 }
 
 export default LoginContainer;
-=======
-// import { connect } from 'react-redux';
-import Login from './Login';
-
-class HomeContainer extends Component {
-
-  render() {
-    return <Login />;
-  }
-}
-
-export default HomeContainer;
->>>>>>> 69d7d7f6dcbeab4498c92477e3d15960510418b7
