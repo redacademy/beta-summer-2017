9f5f332c671f014a268d95f92d8a20f11a0cdecb
melliflorGunk <Shingleton@live.com>
2017-09-11 10:18:48 -0700

    remove redux form reference

--- js/redux/reducers.js
+++ js/redux/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { NavigationReducer } from '@expo/ex-navigation';
-import { reducer as formReducer } from 'redux-form';
+//import { reducer as formReducer } from 'redux-form';
 import { authReducer } from './modules/auth';
 import { userFormsReducer } from './modules/user-forms';
 
