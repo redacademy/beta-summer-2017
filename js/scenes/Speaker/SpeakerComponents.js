de72f8e792386d7982a2087b553136bbe889f999
melliflorGunk <Shingleton@live.com>
2017-09-20 15:44:49 -0700

    added shortcurciutingjanf

--- js/scenes/Speaker/SpeakerComponents.js
+++ js/scenes/Speaker/SpeakerComponents.js
@@ -69,7 +69,7 @@ export const GoalsList = ({ speakerData }) => (
 
   <ScrollView style={cStyles.goalsContainer}>
     {Object.keys(speakerData.speaker_id.goals).map((key, index) => (
-      speakerData.speaker_id.goals[key].length ? <Text key={index} style={cStyles.goalItem}> - {speakerData.speaker_id.goals[key]}</Text> : null
+      speakerData.speaker_id.goals[key].length && <Text key={index} style={cStyles.goalItem}> - {speakerData.speaker_id.goals[key]}</Text>
     )
     )}
   </ScrollView>
