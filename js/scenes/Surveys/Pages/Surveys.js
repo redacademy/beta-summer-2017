114c838b39c1b26def243c3257aacd2dee3f9133
melliflorGunk <Shingleton@live.com>
2017-09-21 17:21:43 -0700

    fixed faulty rendering of comment screen

--- js/scenes/Surveys/Pages/Surveys.js
+++ js/scenes/Surveys/Pages/Surveys.js
@@ -60,7 +60,7 @@ const Surveys = ({
             completePerc={questions.indexOf(question) + 1}
             totalSteps={questions.length + 1}
           />
-          <RightNavArrow navAction={() => nextQuestionFunc(nextQuestion.question)} />
+          <RightNavArrow navAction={() => nextQuestionFunc((nextQuestion) ? nextQuestion.question : null)} />
         </View>
       </View>
     </ScrollView>
